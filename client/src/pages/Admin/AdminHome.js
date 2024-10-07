28e448ddea48e9fd1ac54eb54fd909569d05b282
Niraj Gupta <nirajthekumar07@gmail.com>
2024-10-07 20:29:05 +0530

    first commit

---
+++ client/src/pages/Admin/AdminHome.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Layout from "../../components/shared/Layout/Layout";
+import { useSelector } from "react-redux";
+
+const AdminHome = () => {
+  const { user } = useSelector((state) => state.auth);
+  return (
+    <Layout>
+      <div className="container">
+        <div className="d-felx flex-column mt-4">
+          <h1>
+            Welcome Admin <i className="text-success">{user?.name}</i>
+          </h1>
+          <h3>Manage Blood Bank App </h3>
+          <hr />
+          <p>
+            Hey, Admin you can manage all the data present about the organisation, donor & recepient.
+            Thank You
+          </p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default AdminHome;
